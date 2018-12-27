b058f8f1b86c9a3cfdf30a9fe08a8e536f977fb0
Ryan Kabia <rkabia12@gmail.com>
2018-12-28 01:07:54 +0300

    [finish #162827960]Created an endpoint that allows a user to answer a question given they pass the questionId in url.

--- app/api/v1/models/questions.py
+++ app/api/v1/models/questions.py
@@ -5,27 +5,33 @@ app=Flask(__name__)
 questions = [{
     'author':'Adam Cole',
     'questionId':'001',
-    'question':'I,m Adam freaking Cole BayBay! and that is undisputed!'
+    'question':'I,m Adam freaking Cole BayBay! and that is undisputed!',
+    'answers':[]
 },{
     'author':'Ciampa',
     'questionId':'002',
-    'question':'I ended the fairytale! baddest sob on the planet.Follow my lead!'
+    'question':'I ended the fairytale! baddest sob on the planet.Follow my lead!',
+    'answers':[]
 },{
     'author':'Alesiter Black',
     'questionId':'003',
-    'question':'You will be absolved of your sins as you fade to black!'
+    'question':'You will be absolved of your sins as you fade to black!',
+    'answers':[]
 },{
     'author':'EC3',
     'questionId':'004',
-    'question':'I,m the best here, i am the best there, i am the best anywhere coz im in the top 1 percent.'
+    'question':'I,m the best here, i am the best there, i am the best anywhere coz im in the top 1 percent.',
+    'answers':[]
 },{
     'author':'Petee Dunn',
     'questionId':'005',
-    'question':'none of the lot could hold a candle to the bruiserweight'
+    'question':'none of the lot could hold a candle to the bruiserweight',
+    'answers':[]
 },{
     'author':'Velveteen Dream',
     'questionId':'006',
-    'question':'Lights! Ambience! Experience! Velveteen Dream!'
+    'question':'Lights! Ambience! Experience! Velveteen Dream!',
+    'answers':[]
 }]
 
 @app.route('/questions',methods=['GET'])
@@ -55,7 +61,8 @@ def post_question():
     post_q = {
         'author':author,
         'question':question,
-        'questionId':questionId
+        'questionId':questionId,
+        'answers':[]
     }
 
     questions.append(post_q)
@@ -73,7 +80,30 @@ def delete_specific(questionId):
             delete_item = 'Error,Entry not found'
     return jsonify({'Message':delete_item})
 
-        
+@app.route('/questions/<questionId>/answers',methods=['POST'])
+def post_answer(questionId):
+
+    data = request.get_json()
+
+    ans_author = data['ans_author']
+    answer = data['answer']
+    answerId = data['answerId']
+
+    post_a ={
+        'ans_author':ans_author,
+        'answer':answer,
+        'answerId':answerId
+    }
+
+    for question in questions:
+        if question['questionId'] == questionId:
+            question['answers'].append(post_a)
+            answered_item = question
+            break
+        else:
+            answered_item='Search error,question not found!'
+
+    return jsonify({'Message':answered_item})
 
 
 
