2524a8ccd157f30937817eba972e456c0cb4b09c
Ryan Kabia <rkabia12@gmail.com>
2018-12-27 13:01:20 +0300

    [finish #162827941]Created simple api endpoint that searches specific question given a user passes an id in url

--- app/api/v1/models/fetch_questions.py
+++ app/api/v1/models/fetch_questions.py
@@ -1,41 +1,47 @@
-
 from flask import Flask,request,jsonify
 
 app=Flask(__name__)
 
 questions = [{
     'author':'Adam Cole',
-    'question_id':'001',
+    'questionId':'001',
     'question':'I,m Adam freaking Cole BayBay! and that is undisputed!'
 },{
     'author':'Ciampa',
-    'question_id':'002',
+    'questionId':'002',
     'question':'I ended the fairytale! baddest sob on the planet.Follow my lead!'
 },{
     'author':'Alesiter Black',
-    'question_id':'003',
+    'questionId':'003',
     'question':'You will be absolved of your sins as you fade to black!'
 },{
     'author':'EC3',
-    'question_id':'004',
-    'question':'I,m the best here, i am the best thier, i am the best anywhere coz im in the top 1 percent.'
+    'questionId':'004',
+    'question':'I,m the best here, i am the best there, i am the best anywhere coz im in the top 1 percent.'
 },{
     'author':'Petee Dunn',
-    'question_id':'005',
+    'questionId':'005',
     'question':'none of the lot could hold a candle to the bruiserweight'
 },{
     'author':'Velveteen Dream',
-    'question_id':'006',
+    'questionId':'006',
     'question':'Lights! Ambience! Experience! Velveteen Dream!'
-}
-]
+}]
 
 @app.route('/questions',methods=['GET'])
 def fetch_all():
     return jsonify({'Message':questions})
 
+@app.route('/questions/<questionId>',methods=['GET'])
+def fetch_specific(questionId):
+    for question in questions:
+        if question['questionId'] == questionId:
+            fetched_item = question
+            break
+        else:
+            fetched_item='Search error, not found!'
 
-
+    return jsonify({'Message':fetched_item})
     
 
 
