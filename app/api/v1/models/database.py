88b8d6b850e0545b448d22616ff062e0b60f3ec3
Ryan Kabia <rkabia12@gmail.com>
2018-12-30 04:38:43 +0300

    [chore #162876718]Created database.py in the models folder to store data structures.

---
+++ app/api/v1/models/database.py
@@ -0,0 +1,25 @@
+Reg_Users = [
+    {
+    'name':'Adam Cole',
+    'email':'uera@nx3',
+    'username':'BayBay',
+    'password':'Undisputed4eva'
+    }
+]
+
+Questions = [
+    {
+    'author':'Adam Cole',
+    'questionId':'001',
+    'question':'Can anyone in the back beat  me?',
+    'answers':[]
+    }
+]
+
+Answers = [
+    {
+        'ans_author':'Ricocette',
+        'answerId':,
+        'answer':'I beat you for the Nrth American title!'
+    }
+]
