705678af148ac9088bff8c5c2bbe2d7cf8eac3fb
Ryan Kabia <rkabia12@gmail.com>
2018-12-30 14:49:17 +0300

    [chore #162876718]Moved mock data from views modules to database.py.

--- app/api/v1/models/database.py
+++ app/api/v1/models/database.py
@@ -1,25 +1,68 @@
-Reg_Users = [
+reg_users = [
     {
     'name':'Adam Cole',
     'email':'uera@nx3',
     'username':'BayBay',
     'password':'Undisputed4eva'
+    },
+    {
+    'name':'Bobby Fish',
+    'email':'bfish@nx3',
+    'username':'Fishbone1',
+    'password':'1Fishyboi'
+    },
+    {
+    'name':'Kyle Oriely',
+    'email':'kocombat@nx3',
+    'username':'CombatKo',
+    'password':'Tapout247'
+    },
+    {
+    'name':'Rodreick Strong',
+    'email':'strong@nx3',
+    'username':'Backbreaker Meshiah',
+    'password':'Endofhartache87'
     }
+    
 ]
 
-Questions = [
-    {
+questions = [{
     'author':'Adam Cole',
     'questionId':'001',
-    'question':'Can anyone in the back beat  me?',
+    'question':'I,m Adam freaking Cole BayBay! and that is undisputed!',
     'answers':[]
-    }
-]
+},{
+    'author':'Ciampa',
+    'questionId':'002',
+    'question':'I ended the fairytale! baddest sob on the planet.Follow my lead!',
+    'answers':[]
+},{
+    'author':'Alesiter Black',
+    'questionId':'003',
+    'question':'You will be absolved of your sins as you fade to black!',
+    'answers':[]
+},{
+    'author':'EC3',
+    'questionId':'004',
+    'question':'I,m the best here, i am the best there, i am the best anywhere coz im in the top 1 percent.',
+    'answers':[]
+},{
+    'author':'Petee Dunn',
+    'questionId':'005',
+    'question':'none of the lot could hold a candle to the bruiserweight',
+    'answers':[]
+},{
+    'author':'Velveteen Dream',
+    'questionId':'006',
+    'question':'Lights! Ambience! Experience! Velveteen Dream!',
+    'answers':[]
+}]
+
 
 Answers = [
     {
-        'ans_author':'Ricocette',
-        'answerId':,
-        'answer':'I beat you for the Nrth American title!'
+    'ans_author':'Ricocette',
+    'answerId':'001',
+    'answer':'I beat you for the Nrth American title!'
     }
 ]
